1861c4439d226acdd92e6f1f7a211344a2eb5c6b
kob <mako0844420@gmail.com>
2017-08-12 13:36:45 +0900

    ざっくりOrmaテストケース作成

--- app/src/androidTest/java/eijenson/braveflontiercarendar/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/eijenson/braveflontiercarendar/ExampleInstrumentedTest.kt
@@ -31,14 +31,23 @@ class ExampleInstrumentedTest {
     @Test
     fun 正常登録() {
         val id = repository.insert(BraveNews(title = "title", detail = "detail", period = "period"))
+        Assert.assertNotEquals(-1,id)
+        val lastData = repository.select(id)
+        Assert.assertEquals("title", lastData?.title)
+        Assert.assertEquals("detail", lastData?.detail)
+        Assert.assertEquals("period", lastData?.period)
+    }
+
+    @Test
+    fun テスト(){
+        val id = repository.insert(BraveNews(title = "title", detail = "detail", period = "period"))
         val lastData = repository.select(id)
         Assert.assertEquals("title", lastData?.title)
     }
 
     @Test
-    fun insertTest2() {
-        val id = repository.insert(BraveNews(title = "title", detail = "detail", period = "period"))
-        val lastData = repository.select(-1)
-        Assert.assertEquals(null, lastData?.title)
+    fun 検索で0件の場合はnull() {
+        val nullData = repository.select(-1)
+        Assert.assertEquals(null, nullData)
     }
 }
