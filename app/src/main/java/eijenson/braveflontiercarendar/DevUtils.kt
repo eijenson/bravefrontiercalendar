6c9591397d8a3b3d94c6af39d8c1d2fa6122f912
kob <mako0844420@gmail.com>
2017-09-10 06:59:10 +0900

    プログレスバーの導入

---
+++ app/src/main/java/eijenson/braveflontiercarendar/DevUtils.kt
@@ -0,0 +1,7 @@
+package eijenson.braveflontiercarendar
+
+/**
+ * Created by eijenson on 2017/09/10.
+ */
+class DevUtils {
+}
---
+++ app/src/main/java/eijenson/braveflontiercarendar/message/RxBus.kt
@@ -0,0 +1,7 @@
+package eijenson.braveflontiercarendar.message
+
+/**
+ * Created by eijenson on 2017/09/10.
+ */
+class RxBus {
+}
