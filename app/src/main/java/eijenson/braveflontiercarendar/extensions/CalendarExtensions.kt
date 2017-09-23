8e415bd8a0a1d1f1e01dc574d6bb034912aa71b2
kob <mako0844420@gmail.com>
2017-09-23 18:56:58 +0900

    カレンダー生成処理で変な処理をしていたので変更

--- app/src/main/java/eijenson/braveflontiercarendar/extensions/CalendarExtensions.kt
+++ app/src/main/java/eijenson/braveflontiercarendar/extensions/CalendarExtensions.kt
@@ -4,6 +4,7 @@ import java.util.*
 
 /**
  * Created by eijenson on 2017/08/19.
+ * カレンダーの追加関数
  */
 
 fun Calendar.getYear(): Int {
@@ -18,10 +19,22 @@ fun Calendar.getDate(): Int {
     return get(Calendar.DATE)
 }
 
-fun Calendar.getDay():String{
+fun Calendar.set(year: Int, month: Int, date: Int, hourOfDay: Int, minute: Int, second: Int, milliSecond: Int) {
+    set(year, month, date, hourOfDay, minute, second)
+    set(Calendar.MILLISECOND, milliSecond)
+}
+
+fun Calendar.truncationTime() {
+    set(Calendar.HOUR_OF_DAY, 0)
+    set(Calendar.MINUTE, 0)
+    set(Calendar.SECOND, 0)
+    set(Calendar.MILLISECOND, 0)
+}
+
+fun Calendar.getDay(): String {
     return "" + getYear() + "/" + (getMonth() + 1) + "/" + getDate()
 }
 
-fun Calendar.printlnDate(){
+fun Calendar.printlnDate() {
     println("" + getYear() + "/" + (getMonth() + 1) + "/" + getDate())
 }
--- app/src/test/java/eijenson/braveflontiercarendar/CalendarRepositoryTest.kt
+++ app/src/test/java/eijenson/braveflontiercarendar/CalendarRepositoryTest.kt
@@ -1,61 +1,38 @@
 package eijenson.braveflontiercarendar
 
-import eijenson.braveflontiercarendar.extensions.getDate
-import eijenson.braveflontiercarendar.extensions.getMonth
-import eijenson.braveflontiercarendar.extensions.getYear
+import eijenson.braveflontiercarendar.extensions.set
 import eijenson.braveflontiercarendar.repository.local.CalendarRepository
-import org.junit.Assert
-import org.junit.Before
+import junit.framework.Assert
 import org.junit.Test
-import java.text.DateFormat
 import java.util.*
 
 /**
  * Created by eijenson on 2017/08/18.
+ * カレンダーレポジトリのテスト
  */
 class CalendarRepositoryTest {
 
-    lateinit var c: Calendar
-
-    @Before
-    fun before() {
-        c = Calendar.getInstance()
+    @Test
+    fun success_1970_1_1_first_date_is_1969_12_28() {
+        val cal = Calendar.getInstance()
+        cal.set(1970, 0, 1)
+        val repository = CalendarRepository(cal.time)
+        val list = repository.getCalendar()
+        val first = list.first()
+        cal.set(1969, 11, 28, 0, 0, 0, 0)
+        Assert.assertEquals(first.time, cal.time.time)
     }
 
     @Test
-    fun getCalendarTest() {
-        val list = CalendarRepository().getCalendar()
-        list.map {
-            println(it)
-        }
+    fun success_2017_12_31_last_date_is_2018_1_6() {
+        val cal = Calendar.getInstance()
+        cal.set(2017, 11, 31)
 
-        Assert.assertEquals(c.time.month, Date().month)
+        val repository = CalendarRepository(cal.time)
+
+        val list = repository.getCalendar()
+        val first = list.last()
+        cal.set(2018, 0, 6, 0, 0, 0, 0)
+        Assert.assertEquals(first.time, cal.time.time)
     }
-
-    @Test
-    fun getCalendarTest2() {
-        val date = DateFormat.getDateInstance(DateFormat.DEFAULT, Locale.JAPAN).parse("2016/8/16")
-        val list = CalendarRepository(date).getCalendar()
-        list.map {
-            p(it)
-        }
-
-        Assert.assertEquals(c.time.month, Date().month)
-    }
-
-    fun p(date: Date) {
-        val c = Calendar.getInstance()
-        c.time = date
-        println("" + c.getYear() + "/" + (c.getMonth() + 1) + "/" + c.getDate())
-    }
-
-    fun p(c: Calendar) {
-        print("Date:")
-        print(c.get(Calendar.YEAR))
-        print("/")
-        print(c.get(Calendar.MONTH) + 1)
-        print("/")
-        println(c.get(Calendar.DATE))
-    }
-
 }
