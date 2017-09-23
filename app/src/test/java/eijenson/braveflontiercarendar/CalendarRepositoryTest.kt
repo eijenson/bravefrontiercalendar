5eb7c5db2897188ef15a1fe29f9d1e18eba42836
kob <mako0844420@gmail.com>
2017-09-23 19:14:38 +0900

    before-assert的な感じで空白行を設けた

--- app/src/test/java/eijenson/braveflontiercarendar/CalendarRepositoryTest.kt
+++ app/src/test/java/eijenson/braveflontiercarendar/CalendarRepositoryTest.kt
@@ -17,7 +17,9 @@ class CalendarRepositoryTest {
         val cal = Calendar.getInstance()
         cal.set(1970, 0, 1)
         val repository = CalendarRepository(cal.time)
+
         val list = repository.getCalendar()
+
         val first = list.first()
         cal.set(1969, 11, 28, 0, 0, 0, 0)
         Assert.assertEquals(first.time, cal.time.time)
@@ -27,10 +29,10 @@ class CalendarRepositoryTest {
     fun success_2017_12_31_last_date_is_2018_1_6() {
         val cal = Calendar.getInstance()
         cal.set(2017, 11, 31)
-
         val repository = CalendarRepository(cal.time)
 
         val list = repository.getCalendar()
+
         val first = list.last()
         cal.set(2018, 0, 6, 0, 0, 0, 0)
         Assert.assertEquals(first.time, cal.time.time)
