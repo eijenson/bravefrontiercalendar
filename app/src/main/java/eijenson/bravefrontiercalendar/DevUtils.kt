0a7adb104a342d025a6cfdd86dc6d5ba83045337
kob <mako0844420@gmail.com>
2017-11-12 11:17:39 +0900

    コメントの削除

--- app/src/main/java/eijenson/bravefrontiercalendar/DevUtils.kt
+++ app/src/main/java/eijenson/bravefrontiercalendar/DevUtils.kt
@@ -71,24 +71,16 @@ object DevUtils {
         mBuilder.setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle("My notification")
                 .setContentText("Hello World!")
-        // Creates an explicit intent for an Activity in your app
         val resultIntent = Intent(context, MainActivity::class.java)
 
-        // The stack builder object will contain an artificial back stack for the
-        // started Activity.
-        // This ensures that navigating backward from the Activity leads out of
-        // your application to the Home screen.
         val stackBuilder = TaskStackBuilder.create(context)
-        // Adds the back stack for the Intent (but not the Intent itself)
         stackBuilder.addParentStack(MainActivity::class.java)
-        // Adds the Intent that starts the Activity to the top of the stack
         stackBuilder.addNextIntent(resultIntent)
         val resultPendingIntent = stackBuilder.getPendingIntent(
                 0,
                 PendingIntent.FLAG_UPDATE_CURRENT
         )
         mBuilder.setContentIntent(resultPendingIntent)
-        // mId allows you to update the notification later on.
         manager.notify(0, mBuilder.build())
     }
 }
