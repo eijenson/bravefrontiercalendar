e5696dc56233e5ae89ed61e8380aecc6c32ca7e8
kob <mako0844420@gmail.com>
2017-11-23 12:46:20 +0900

    パッケージ構成の変更

--- app/src/main/java/eijenson/bravefrontiercalendar/repository/models/Progress.kt
+++
@@ -1,7 +0,0 @@
-package eijenson.bravefrontiercalendar.repository.models
-
-/**
- * Created by eijenson on 2017/09/25.
- * プログレスバーの進捗と最大値を格納するデータクラス
- */
-data class Progress(val now: Int, val max: Int)
